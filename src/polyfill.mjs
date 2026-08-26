54cac9ecac925017d5ab83e28b9e245f0b19806f
Jon Edgeworth <jonryanedge@gmail.com>
2026-08-25 21:34:06 -0400

    Add dispatchEvent + CustomEvent to document stub

--- src/polyfill.mjs
+++ src/polyfill.mjs
@@ -4,9 +4,20 @@ if (!globalThis.document) {
   globalThis.document = {
     addEventListener: () => {},
     removeEventListener: () => {},
+    dispatchEvent: () => true,
     createElement: () => ({ style: {}, getContext: () => null }),
     querySelector: () => null,
     body: {},
+  };
+}
+
+if (!globalThis.CustomEvent) {
+  globalThis.CustomEvent = class CustomEvent {
+    constructor(type, options = {}) {
+      this.type = type;
+      this.detail = options.detail;
+      this.bubbles = options.bubbles || false;
+    }
   };
 }
 
